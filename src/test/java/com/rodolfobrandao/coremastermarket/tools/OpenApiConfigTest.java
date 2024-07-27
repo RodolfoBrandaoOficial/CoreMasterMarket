13eb7fad4a259116d68f66f0f580041ad5c2f25e
RodolfoBrandaoOficial <31350371+RodolfoBrandaoOficial@users.noreply.github.com>
2024-07-27 16:45:41 -0300

    Integração do Swagger, ajuste e testing

---
+++ src/test/java/com/rodolfobrandao/coremastermarket/tools/OpenApiConfigTest.java
@@ -0,0 +1,12 @@
+package com.rodolfobrandao.coremastermarket.tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenApiConfigTest {
+
+    @Test
+    void customOpenAPI() {
+    }
+}
